df024365aa5ad90bfb3ebb18c0e71f2fc30381e7
Dmitry_Zhurov <you@example.com>
2022-09-26 18:45:32 +0300

    update the provider

--- development/providers.tf
+++ development/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.22.0"
+    }
+  }
+}
 provider "google" {
   credentials = "ethereal-jigsaw-362607-1921c60b2d6d.json"
   project     = "ethereal-jigsaw-362607"
--- modules/application/provider.tf
+++ modules/application/provider.tf
@@ -1,4 +1,18 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+data "google_service_account_access_token" "my_kubernetes_sa" {
+  target_service_account = "gketerraform"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "3600s"
+}
 
+data "google_container_cluster" "my_cluster" {
+  name     = "my-cluster"
+  location = "us-central1"
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_service_account_access_token.my_kubernetes_sa.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
 }
